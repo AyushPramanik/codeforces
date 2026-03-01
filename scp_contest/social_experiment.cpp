02ef2e120ecad90ba532cf02397f763dc3a92f36
Ayush Pramanik <ayushpramanik2007@gmail.com>
2026-02-28 21:05:03 -0500

    SCP Contest #1

---
+++ scp_contest/social_experiment.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// SCP Contest - A. Social Experiment
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n <= 3)
+            cout << n << endl;
+        else
+            cout << n % 2 << endl;
+    }
+
+    return 0;
+}
